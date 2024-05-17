24eaa5c3e3d57d4190de88fd702b539b06de8569
Auto Logon <autologon@CSED.LAB>
2024-05-17 11:34:33 -0300

    FRONT - Exercício do contador

--- front-ambientacao/src/App.tsx
+++ front-ambientacao/src/App.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
+import Soma from './Soma';
 
+//1 - O nome do componente OBRIGATORIAMENTE deve
+//começar com a primeira letra em maiúsculo
+//2 - O componete DEVE ser uma função do JS/TS
+//3 - O componente DEVE retornar APENAS UM elemento
+//pai HTML
 function App() {
   return (
     <div>
-      <h1>Minha primeira aplicação em React!</h1>
+      <Soma></Soma>
     </div>
   );
 }
-
+//4 - O componente DEVE ser exportado
 export default App;
